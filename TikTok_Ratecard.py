aa2b9489b9a3f62ace42991bbadf150d044b301c
Andi Darmawan <92540957+andidar83@users.noreply.github.com>
2024-05-31 11:40:37 +0700

    Update TikTok_Ratecard.py

--- TikTok_Ratecard.py
+++ TikTok_Ratecard.py
@@ -26,16 +26,6 @@ import glob
 import numpy as np
 
 credentials ={
-  "type": "service_account",
-  "project_id": "project-01-322003",
-  "private_key_id": "231cd74240848f5b9bfc5b781c51ec109d9edbd5",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQCQ7amh4vUO2J1h\ntTFefuhalmw2oQFfrMB+zHAEOS0ddV1/npq4kFHqrjw34aaRe61nQ+I7wUfuUEUu\ncouf+ll76UfIoJKyXD5UcI7lGYPJWAjGaPABg85kEGCpTrf8YS4afdNI13Rtwi3o\nsbIFt37S6AGY9AALcLaS5TA9WY30M6EDaUnGhCEHWPW+BLPkeRHspUUcUK2+U/zA\nUVT0klbOQ2NxqGbkyBjzqdz/GD4bDPmJiQ9Rs0m86OumI5EUtsj7m7509Qj6PZ4w\nZR/gd+xMiocpxYl2Q1D5W3e+6k3AZ/Uy4Av+zs0XW+2VfsZtwm+8A8NLb/La0c6C\nijdTGYplAgMBAAECgf8/udrDfu655UFwzsyIpu+VbhevqXGCQmtREbXrWLBCCAyU\nxFN3S7oOsuO2nZu6B31AfAfktf6vS3pTPuidq0ytnogcljpRVT41JycDYsXLukfd\nOr60Ujghtqt01/2XN6yOCpJ/KD3fFlhdvYCSaVjJuY/HD4M5yNX2WyX0oMoXSWp3\nUDRB4YBdLTuVFJctGBc6pLSZIrGk8f6zblkOE6OfYUQ3m0xX83bfjfhHRNUtE6ql\nYYmjUxB4UDr0MYi/W05U/y60BVH0dFsPYRqeVMWkKrl3VMyW0Q9lv19+I0YCitq5\nnetuNZAeW3WeGM1nmkPZVM3Y0wViD7AnI/tHTaECgYEAw5yFXToXl5bYhMCEc4FZ\naMV9qRdQvbc3I6UphEkVTbdeG+LCQokI0aap58CCmdBxqFQe1jzckCKnGlM0bhrr\n72Dewd+ZtRLm9cUzeT6+DeiFBU6w4pzH6wbIZyNW6QLPWyE6MzsmjWTlCCQrerih\nfnGFzaiiQoSSA8b+MYR28/ECgYEAvauUsp98UpraDXkcs/CdRbekFOmxHckDcHaY\nbdlygv0T7PmyQUc+PLVXy8dnX8+ztLDyowQr7T1eUgrwc9gnqu7gqKpFq3CuuK8c\n9iiBt6Pp/c0BZl4ISXznlLkxEq9O7D3om1QnxVt3x7UyMtbZ+vPtzkQOWV6g0ImZ\n0SddIbUCgYAgT1Uz/elxr4fZ/ZajIYVsKdrEuEYs3/tqlthRsmSjbptLzdu6c7oS\nLw43anPoBicP370sM+dWo3KohX/OhAHei0igC1fvMc0WYVlMOJHZ6EM4ijPramwX\nJQqrBopPeJhZfBaJ6cZHapfuDVNlNOPv575rJuSOSil4GEFaWn/vcQKBgAKGDexy\ngsz2koArZF5gjTcoQl6k3V5wFkOg0FZl1kg35dP5Xo3PzNwx7YEgnheAQ/vbft78\nZzk439BnpNtlvOFhXEviOBsX3LtIWTJNR6yR2cMuvx4FkkaoCszHygddncc266ll\nbUIK/YUCvDBA+5WDOV7NOQOnZqxfqFRIGc0hAoGBAIe44ZpXPPlyKhtsY/Yn2rV+\nawutAkct4gFUBV+CGcAsRvvbR4iCpoW83M9bLYepj/TvZyUAvDS2JpV1t+lDMjpn\nN5WZb48bZGphDmFiYcrYgURduqR8j3PhSUXGyCEruaTVpbFDkvxS0X2mysBNJ94q\n4xHuOkBDG9EP7V7UDjPN\n-----END PRIVATE KEY-----\n",
-  "client_email": "g-sheets@project-01-322003.iam.gserviceaccount.com",
-  "client_id": "111359721706864293131",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/g-sheets%40project-01-322003.iam.gserviceaccount.com"
 }
 gc= gs.service_account_from_dict(credentials)
 
